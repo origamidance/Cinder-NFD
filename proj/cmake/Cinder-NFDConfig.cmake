1d472e417186277305908c5447e30e30868807c9
origamidance <origamidance@gmail.com>
2017-04-02 13:37:37 +0200

    update cmake

--- proj/cmake/Cinder-NFDConfig.cmake
+++ proj/cmake/Cinder-NFDConfig.cmake
@@ -2,16 +2,24 @@ if(NOT TARGET Cinder-NFD)
   get_filename_component( CINDER-NFD_PATH "${CMAKE_CURRENT_LIST_DIR}/../.." ABSOLUTE)
 	get_filename_component( CINDER_PATH "${CMAKE_CURRENT_LIST_DIR}/../../../../Cinder" ABSOLUTE )
   message("CINDER PATH= ${CINDER_PATH}")
-  list( APPEND CINDER-NFD_SOURCES
-    ${CINDER-NFD_PATH}/src/nativefiledialog/src/nfd_common.c
-    ${CINDER-NFD_PATH}/src/nativefiledialog/src/nfd_gtk.c
-    )
-  find_package(PkgConfig REQUIRED)
-  pkg_check_modules(GTK3 REQUIRED gtk+-3.0)
-  include_directories("${CINDER-NFD_PATH}/src/nativefiledialog/src/include")
-  include_directories(${GTK3_INCLUDE_DIRS})
-  link_directories(${GTK3_LIBRARY_DIRS})
-  add_definitions(${GTK3_CFLAGS_OTHER})
+  if (UNIX AND NOT APPLE)
+      list( APPEND CINDER-NFD_SOURCES
+              ${CINDER-NFD_PATH}/src/nativefiledialog/src/nfd_common.c
+              ${CINDER-NFD_PATH}/src/nativefiledialog/src/nfd_gtk.c
+              )
+      find_package(PkgConfig REQUIRED)
+      pkg_check_modules(GTK3 REQUIRED gtk+-3.0)
+      include_directories("${CINDER-NFD_PATH}/src/nativefiledialog/src/include")
+      include_directories(${GTK3_INCLUDE_DIRS})
+      link_directories(${GTK3_LIBRARY_DIRS})
+      add_definitions(${GTK3_CFLAGS_OTHER})
+  elseif(APPLE)
+      list( APPEND CINDER-NFD_SOURCES
+              ${CINDER-NFD_PATH}/src/nativefiledialog/src/nfd_common.c
+              ${CINDER-NFD_PATH}/src/nativefiledialog/src/nfd_cocoa.m
+              )
+      include_directories("${CINDER-NFD_PATH}/src/nativefiledialog/src/include")
+  endif ()
   add_library(Cinder-NFD ${CINDER-NFD_SOURCES})
   target_include_directories( Cinder-NFD PUBLIC
     "${CINDER-NFD_PATH}/src/include")
@@ -23,5 +31,8 @@ if(NOT TARGET Cinder-NFD)
 		  "$ENV{CINDER_PATH}/${CINDER_LIB_DIRECTORY}" )
 	endif()
   target_link_libraries(Cinder-NFD PRIVATE cinder)
-  target_link_libraries(Cinder-NFD PUBLIC ${GTK3_LIBRARIES})
+  if (UNIX AND NOT APPLE)
+      target_link_libraries(Cinder-NFD PUBLIC ${GTK3_LIBRARIES})
+  elseif()
+  endif ()
 endif()
